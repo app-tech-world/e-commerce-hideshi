9b050f6df5aba89137d5a0c378923d78ddad37b7
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-06-13 23:05:19 -0500

    color pallete

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,9 @@
+import HomePage from "@/components/home/HomePage";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>E-COMMERCE APPLICATION</h1>
+    <main>
+      <HomePage />
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,32 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      phone: "375px",
+      tablet: "768px",
+      notebook: "1280px",
+      desktop: "1440px"
     },
+    textColor: {
+      skin: {
+        primary: "var(--fs-color-neutral-7)",
+        secondary: "var(--fs-color-neutral-7)",
+        success: "#50b191"
+      }
+    },
+    backgroundColor: {
+      skin: {
+        primary: "var(--fs-color-main-3)",
+        light: "var(--fs-color-main-2)",
+        "tertiary-hover": "var(--fs-color-main-2)",
+        "tertiary-active": "var(--fs-color-main-3)",
+        warning: "#b17000",
+        success: "#007043",
+        disabled: "var(--fs-color-main-3)"
+
+      }
+    }
   },
   plugins: [],
 }
+
